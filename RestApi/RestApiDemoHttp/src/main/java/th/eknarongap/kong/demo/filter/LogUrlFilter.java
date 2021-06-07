0d7148ac71fc5b72a2db7d4962dd4e461495e7fb
EknarongAphiphutthikul <akenarongake@hotmail.com>
2021-06-07 23:58:40 +0700

    add log url

---
+++ RestApi/RestApiDemoHttp/src/main/java/th/eknarongap/kong/demo/filter/LogUrlFilter.java
@@ -0,0 +1,29 @@
+package th.eknarongap.kong.demo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogUrlFilter implements Filter {
+	
+	Logger logger = LoggerFactory.getLogger(LogUrlFilter.class);
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		logger.info(httpServletRequest.getRequestURL().toString());
+		chain.doFilter(request, response);
+	}
+
+}
